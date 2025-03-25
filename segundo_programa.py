3d8a002d65586a6c37335ee5316441e5c9a1929b
Stick2024 <santiagohbello2015@gmail.com>
2025-03-25 11:27:46 -0500

    se suben los archivos de py

---
+++ segundo_programa.py
@@ -0,0 +1,5 @@
+#este es el inicio del programa
+nombre = "Erick Bello"
+Edad = 25
+
+print(f"mi nombre es {nombre} y tengo {Edad} años.")
---
+++ tercer_programa.py
@@ -0,0 +1,16 @@
+# Esta imprime el tipo de docmento para registro
+print ("Hola mundo")
+
+#Entrada de datos 
+name = input("Ingrese un nombre: ")
+age = input("Ingrese la edad: ")
+
+#salida de datos
+print(f"Mi nombre es {name} y tengo {age} años.")
+
+#entrada de datos por script
+import sys
+
+data = sys.argv
+
+print(f"Hola data")
