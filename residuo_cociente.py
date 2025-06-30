6497f45fbf6fdeb0de9afb5af8d93236695c54bd
Stick2024 <santiagohbello2015@gmail.com>
2025-06-29 21:48:40 -0500

    nuevo ejercicio

---
+++ residuo_cociente.py
@@ -0,0 +1,13 @@
+print('Calcular cociente y residuo')
+
+a = input('Ingrese primer número: ')
+b = input('Ingrese segundo número:')
+
+a = int(a)
+b = int(b)
+
+cociente = a // b
+residuo = a % b
+
+print('El cociente es: ', cociente)
+print('El Residuo es: ', residuo)
