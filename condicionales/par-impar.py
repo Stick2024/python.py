6c4bf80fdaf8aa868cf3db5d41c95051145706f0
Stick2024 <santiagohbello2015@gmail.com>
2025-06-30 19:29:52 -0500

    Ejercicio par y impar repo

---
+++ condicionales/par-impar.py
@@ -0,0 +1,20 @@
+#Este es un ejercicio solo remueve el "#" del ejercicio a y b
+#a = 10
+#b = 5
+#if a > b:
+#    print("a es mayor que b")
+
+#Este es un ejercicio solo remueve 
+#el "#" del ejercicio "number" y "if" y "else" como los "print"
+
+#number = 11
+#if number % 2 == 0:
+#    print(f"El numero {number} es par.")
+#else:
+#    print(f"El numero {number} es impar.")
+
+numero = int(input("Ingrese un numero: "))
+if numero % 2 == 0:
+    print(f"El numero {numero} es par.")
+else:
+    print(f"El numero {numero} es impar.")
