9812f955b075f4cf4c48b2db003297639c66d16e
Stick2024 <santiagohbello2015@gmail.com>
2025-07-20 22:00:22 -0500

    se sube el uso cadenas

---
+++ Uso-cadenas/Cadenas.py
@@ -0,0 +1,17 @@
+simple_string = 'Esto es con cadena de 1 guion'
+double_sting = "Esto es con python"
+
+
+Text = """Esto es un parrafo de texto"""
+Text1 = """Esto es un parrafo de texto"""
+Text2 = """Esto es un parrafo de texto"""
+Text3 = '''Es un eje,mplo de un parrafo de texto'''
+
+print(simple_string)
+print (double_sting)
+print(Text)
+print (Text1)
+print (Text2)
+print (Text3)
+
+print("Hola me" + "gusta python")
